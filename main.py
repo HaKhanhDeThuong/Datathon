d0c7b5c835b209aceb47ab0136d8b167ded2d2a4
vietcongchan <chinguyentv2003@gmail.com>
2023-12-16 15:44:55 +0700

    datathon-updated

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from Yolo_Detector import ObjectDetector
+from detector.Yolo_Detector import ObjectDetector
 import threading
 import cv2
 
 if __name__ == "__main__":
     Detector = ObjectDetector()
-    print(Detector.detect(r"D:\code_folder\data-code\Datathon2023\git\Datathon\data\customer_behaviors_cctv_mentor_data\front\OneLeaveShop1\frame\OneLeaveShop1front0196.jpg"))
+    Detector.mp4_loader(r"D:\code_folder\data-code\Datathon2023\git\Datathon\data-old\customer_behaviors_cctv_mentor_data\cor\OneStopMoveEnter1\OneStopMoveEnter1.mpg")
+    #Detector.video_capture()
     #thread = threading.Thread(target=mp4_loader, args=(Detector, "1.mp4"))
     #thread.start()
 
